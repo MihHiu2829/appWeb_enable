fcb3ae5e057af63b19682ef4f012457ba5a76a85
Minh_Hieu <himayato.2019.vn@gmail.com>
2022-08-26 10:58:05 +0700

    Update tiến độ dự án bản thân.lừi quá quí dị, chắc phải qa tuần mới xong

--- app/src/main/java/com/example/layputdesgin/MainActivity.java
+++ app/src/main/java/com/example/layputdesgin/MainActivity.java
@@ -3,13 +3,16 @@ package com.example.layputdesgin;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Dialog;
+import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.Gravity;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.view.animation.AnimationUtils;
 
 import com.example.layputdesgin.databinding.ActivityMainBinding;
 
@@ -19,26 +22,16 @@ public class MainActivity extends baseAct<ActivityMainBinding> {
 
     @Override
     protected void initViews() {
-        binding.tvHome.setOnClickListener(v -> GotoHome());
+                binding.tvAboutMe01.setOnClickListener(v -> gotoAboutme(v));
     }
 
-    private void GotoHome() {
-        Dialog dialog = new Dialog(this);
-        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE) ;
-        dialog.setContentView(R.layout.act_home);
-
-        Window window = dialog.getWindow() ;
-        if(window == null ) return ;
-        window.setLayout(WindowManager.LayoutParams.MATCH_PARENT,WindowManager.LayoutParams.WRAP_CONTENT);
-        window.setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
-
-        WindowManager.LayoutParams windowAttributes = window.getAttributes() ;
-        windowAttributes.gravity = Gravity.CENTER ;
-        window.setAttributes(windowAttributes);
-        dialog.setCancelable(true);
-//        dialog.dismiss();
-            dialog.show();
+    private void gotoAboutme(View v) {
+        v.startAnimation(AnimationUtils.loadAnimation(this, androidx.appcompat.R.anim.abc_fade_in));
+        Intent intent = new Intent() ;
+        intent.setClass(this,m002_act.class);
+        startActivity(intent);
     }
+
 
     @Override
     protected ActivityMainBinding initViewBinding() {
